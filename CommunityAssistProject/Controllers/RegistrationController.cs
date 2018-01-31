5780d223da9bce81c6f0ac93019fb92d933fa2bc
Sean Gilliland <gillilands19@gmail.com>
2018-01-30 23:37:49 -0800

    Assignment Four: Registration

---
+++ CommunityAssistProject/Controllers/RegistrationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistProject.Models;
+
+namespace CommunityAssistProject.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+        NewPerson newPerson = new NewPerson();
+
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+        public ActionResult Success()
+        { 
+            Message successMessage = new Message();
+            successMessage.MessageText = "Registration Successful";
+            return View("result", successMessage);
+        }
+
+        public ActionResult Failure()
+        {
+            Message failureMessage = new Message();
+            failureMessage.MessageText = "Registration Failed";
+            return View("result", failureMessage);
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include ="lastname, firstname, email, password, apartmentNumber, street, city, state, zipcode, phone")]NewPerson newPerson)
+        {
+            int result = db.usp_Register(newPerson.LastName, newPerson.FirstName, newPerson.Email, newPerson.PlainPassword, newPerson.ApartmentNumber , newPerson.Street, newPerson.City, newPerson.State, newPerson.ZipCode, newPerson.Phone);
+
+            if (result != -1)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return RedirectToAction("Failure");
+        }
+
+    }
+}
---
+++ CommunityAssistProject/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssistProject.Models
+{
+    public class Message
+    {
+        public string MessageText { get; set; }
+    }
+}
